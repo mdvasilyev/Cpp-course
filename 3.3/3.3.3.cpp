145160f82b7c33265200c36ab66f7bc4ab9d66f2
Maxim Vasilyev <mayxis@inbox.ru>
2023-07-14 22:19:03 +0300

    accepted version

--- 3.3/3.3.3.cpp
+++ 3.3/3.3.3.cpp
@@ -4,97 +4,103 @@
 #include <cstring> // strlen, strcpy
 using namespace std;
 
-// struct String {
-// 	String(const char *str = ""){
-//         int length = strlen(str);
-//         this->size = length;
-//         char *copy = new char[length + 1];
-//         strcpy(copy, str);
-//         this->str = copy;
-//         delete []copy;
-//     }
-	// String(size_t n, char c){
-    //     char *str = new char[n + 1];
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         str[i] = c;
-    //     }
-    //     str[n] = '\0';
-    //     this->size = n;
-    //     this->str = str;
-    // }
-
-	// ~String(){
-    //     delete [] str;
-    // }
+struct String
+{
+    String(const char *str = "")
+    {
+        int length = strlen(str);
+        this->size = length;
+        this->str = new char[length + 1];
+        strcpy(this->str, str);
+    }
+    String(size_t n, char c) : size(n), str(new char[n + 1])
+    {
+        for (int i = 0; i < n; i++)
+        {
+            str[i] = c;
+        }
+        str[n] = '\0';
+    }
+    ~String()
+    {
+        delete[] str;
+    }
 
     // /* Реализуйте этот метод. */
-	// void append(String &other){
-    //     int other_size = strlen(other.str);
-    //     int new_len = this->size + other_size + 1;
-    //     char *new_str = new char[new_len];
-    //     strcpy(new_str, this->str);
-    //     strcpy(new_str + this->size - 1, other.str);
-    //     // new_str[new_len - 1] = '\0';
-    //     this->str = new_str;
-    //     delete [] new_str;
-    // }
+    void append(String &other)
+    {
+        int old_size = this->size;
+        int other_size = strlen(other.str);
+        int new_size = old_size + other_size + 1;
+        char *new_str = new char[new_size];
+        for (int i = 0; i < new_size; i++)
+        {
+            if (i < old_size)
+            {
+                new_str[i] = this->str[i];
+            }
+            else
+            {
+                new_str[i] = other.str[i - old_size];
+            }
+        }
+        this->size = new_size - 1;
+        delete[] this->str;
+        this->str = new_str;
+    }
 
-// 	size_t size;
-// 	char *str;
-// };
+    size_t size;
+    char *str;
+};
 
-int main() {
-    // const char *str = "Hello, ";
-    // String s1(str);
-    // String s2("World!");
-    int a = 1;
-    cout << a << endl;
+int main()
+{
+    const size_t ntest = 10;
 
-    // s1.append(s2);
-    // std::cout << s1 << std::endl;
+    std::string tests[ntest][2] = {
+        {"", ""},
+        {"", "test"},
+        {"test", ""},
+        {"test", "test"},
+        {"Hello ", " world!"},
+        {"Supercalifragilistic", "expialidocious"}};
 
-    // const size_t ntest = 10;
-    
-    // std::string tests[ntest][2] = {
-    //                             {"", ""},
-    //                             {"", "test"},
-    //                             {"test", ""},
-    //                             {"test", "test"},
-    //                             {"Hello ", " world!"},
-    //                             {"Supercalifragilistic", "expialidocious"}
-    //                           };
-    
-    // for (size_t i=0; i<ntest; ++i) {
-    //     String t1(tests[i][0].c_str());
-    //     String t2(tests[i][1].c_str());
-        
-    //     t1.append(t2);
-        
-    //     std::string res(t1.str);
-    //     if (res != tests[i][0]+tests[i][1]) {
-    //         std::cout << "Test " << i+1 << " failed!" << std::endl;
-    //         std::cout << "Must be " << tests[i][0]+tests[i][1] << std::endl;
-    //         std::cout << "But result is " << res << std::endl;
-    //     } else {
-    //         std::cout << "Test " << i+1 << " passed!" << std::endl;
-    //     }
-        
-    // }
-    
-    // std::string last = "Same pointer test";
-    // String t(last.c_str());
-    // t.append(t);
-    
-    // std::string res(t.str);
-    
-    // if (res != last+last) {
-    //     std::cout << "Test " << ntest+1 << " failed!" << std::endl;
-    //     std::cout << "Must be " << last+last << std::endl;
-    //     std::cout << "But result is " << res << std::endl;
-    // } else {
-    //     std::cout << "Test " << ntest+1 << " passed!" << std::endl;
-    // }
-        
+    for (size_t i = 0; i < ntest; ++i)
+    {
+        String t1(tests[i][0].c_str());
+        String t2(tests[i][1].c_str());
+
+        t1.append(t2);
+
+        std::string res(t1.str);
+        if (res != tests[i][0] + tests[i][1])
+        {
+            std::cout << "Test " << i + 1 << " failed!" << std::endl;
+            std::cout << "Must be " << tests[i][0] + tests[i][1] << std::endl;
+            std::cout << "But result is " << res << std::endl;
+        }
+        else
+        {
+            std::cout << "Test " << i + 1 << " passed!" << std::endl;
+        }
+    }
+
+    std::string last = "Same pointer test";
+    String t(last.c_str());
+    t.append(t);
+
+    std::string res(t.str);
+
+    if (res != last + last)
+    {
+        std::cout << "Test " << ntest + 1 << " failed!" << std::endl;
+        std::cout << "Must be " << last + last << std::endl;
+        std::cout << "But result is " << res << std::endl;
+    }
+    else
+    {
+        std::cout << "Test " << ntest + 1 << " passed!" << std::endl;
+    }
+
     return 0;
 }
